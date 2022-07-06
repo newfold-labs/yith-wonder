f02f26f9a621e647c61e5cbe459c5c9fcb7ea184
francgrasso <francgrasso@gmail.com>
2022-07-06 21:51:11 +0200

    home-1, home-2, home-3 WIP templates. Fix no-title template spacing

---
+++ patterns/homepage-1.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * Title: Homepage #1
+ * Slug: wonder/homepage-1
+ * Categories: wonder-pages
+ * Block Types: core/post-content
+ *
+ * @package wonder
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"full","style":{"color":{"background":"#e8e8e8"},"spacing":{"blockGap":"0px","padding":{"top":"0px","bottom":"0px"}}},"layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#e8e8e8;padding-top:0px;padding-bottom:0px"><!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":null} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"huge"} -->
+<h2 class="has-primary-color has-text-color has-huge-font-size" style="font-style:normal;font-weight:600">New Spring Casuals</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} -->
+<p style="font-style:normal;font-weight:500">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"15px","right":"50px","bottom":"15px","left":"50px"}}},"fontSize":"normal"} -->
+<div class="wp-block-button has-custom-font-size has-normal-font-size"><a class="wp-block-button__link" style="padding-top:15px;padding-right:50px;padding-bottom:15px;padding-left:50px"><strong>Shop now</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:66.66%"><!-- wp:image {"id":371,"sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-large is-style-default"><img src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/header-image-1024x700.jpg" alt="" class="wp-image-371"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"verticalAlignment":null,"align":"full","style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-columns alignfull"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/black-1024x817.jpg","id":387,"dimRatio":0,"minHeight":100,"minHeightUnit":"%","align":"center"} -->
+<div class="wp-block-cover aligncenter" style="min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-387" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/black-1024x817.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","layout":{"inherit":false,"contentSize":"70%"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":3,"textColor":"primary","fontSize":"large"} -->
+<h3 class="has-primary-color has-text-color has-large-font-size">Black is the new black.</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Gumbo beet greens corn soko endive gumbo gourd. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"16px","right":"30px","bottom":"16px","left":"30px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link" href="#" style="padding-top:16px;padding-right:30px;padding-bottom:16px;padding-left:30px"><strong>VIEW OUR ACCESSORIES</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":null,"align":"full","style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"center","layout":{"inherit":false,"contentSize":"70%"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":3,"textColor":"primary","fontSize":"large"} -->
+<h3 class="has-primary-color has-text-color has-large-font-size">Jeans &amp; Denim collection</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Gumbo beet greens corn soko endive gumbo gourd. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"2"}}} -->
+<p style="line-height:2">Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"top":"16px","right":"30px","bottom":"16px","left":"30px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link" href="#" style="padding-top:16px;padding-right:30px;padding-bottom:16px;padding-left:30px"><strong>SHOP NOW</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/denim.jpg","id":394,"dimRatio":0,"minHeight":100,"minHeightUnit":"%","align":"center"} -->
+<div class="wp-block-cover aligncenter" style="min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-394" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/denim.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"large"} -->
+<h3 class="has-text-align-center has-primary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:600">This week’s deals</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:woocommerce/product-new {"columns":5,"rows":1,"alignButtons":true,"contentVisibility":{"image":true,"title":true,"price":true,"rating":false,"button":false}} /-->
+
+<!-- wp:spacer {"height":"60px"} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"className":"has-2-columns"} -->
+<div class="wp-block-columns has-2-columns"><!-- wp:column {"width":"34%","className":"animate-fade-right","layout":{"inherit":true}} -->
+<div class="wp-block-column animate-fade-right" style="flex-basis:34%"><!-- wp:cover {"customOverlayColor":"#f0f0f0","isDark":false} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#f0f0f0"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-group"><!-- wp:image {"id":2952,"width":472,"height":182,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large is-resized"><img src="https://proteo.yithemes.com/classic-shop/wp-content/uploads/sites/2/2019/08/glass@2x.png" alt="" class="wp-image-2952" width="472" height="182"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"very-dark-gray"} -->
+<p class="has-text-align-center has-very-dark-gray-color has-text-color">DISCOVERY THE TREND OF</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":50,"fontStyle":"normal","fontWeight":"800"}},"textColor":"very-dark-gray"} -->
+<p class="has-text-align-center has-very-dark-gray-color has-text-color" style="font-size:50px;font-style:normal;font-weight:800">GLASSES</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66%","className":"animate-fade-left","layout":{"inherit":true}} -->
+<div class="wp-block-column animate-fade-left" style="flex-basis:66%"><!-- wp:cover {"url":"https://proteo.yithemes.com/classic-shop/wp-content/uploads/sites/2/2019/08/Intersection-1@2x.jpg","id":2953,"dimRatio":70,"overlayColor":"secondary","minHeight":100,"minHeightUnit":"%"} -->
+<div class="wp-block-cover" style="min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2953" alt="" src="https://proteo.yithemes.com/classic-shop/wp-content/uploads/sites/2/2019/08/Intersection-1@2x.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"30px"}}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":50,"fontStyle":"normal","fontWeight":"700","lineHeight":1.2}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color" style="font-size:50px;font-style:normal;font-weight:700;line-height:1.2">SAVE 20% IN<br>MAN CLOTHING</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":18}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color" style="font-size:18px">Enjoy a special -20% discount on man category</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/testimonials-bg-scaled.jpg","id":408,"dimRatio":0,"align":"full","style":{"spacing":{"padding":{"top":"100px","right":"100px","bottom":"0px","left":"100px"}}}} -->
+<div class="wp-block-cover alignfull" style="padding-top:100px;padding-right:100px;padding-bottom:0px;padding-left:100px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-408" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/testimonials-bg-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:media-text {"mediaPosition":"right","mediaId":409,"mediaLink":"http://blocktheme.test/wonder/home-1/martharamos/","mediaType":"image"} -->
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/martharamos-789x1024.jpg" alt="" class="wp-image-409 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"huge"} -->
+<p class="has-huge-font-size" style="font-style:normal;font-weight:700">“ Here I can find original items, low prices without to lose quality or sustainability.<br>Love it! “</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p>Martha Ramos <br>Customer from 2007</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+
+<!-- wp:html -->
+<div style="margin-bottom:-100px"></div>
+<!-- /wp:html --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":"150px"} -->
+<div style="height:150px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
---
+++ patterns/homepage-2.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Title: Homepage #2
+ * Slug: wonder/homepage-2
+ * Categories: wonder-pages
+ * Block Types: core/post-content
+ *
+ * @package wonder
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner1.jpg","id":436,"dimRatio":0,"minHeight":680,"isDark":false,"className":"has-custom-content-position is-position-bottom-left ek-linked-block ek-linked-block-animate"} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-left ek-linked-block ek-linked-block-animate" style="min-height:680px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-436" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner1.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"backgroundColor":"primary","textColor":"background"} -->
+<p class="has-background-color has-primary-background-color has-text-color has-background"><strong>A romantic touch of black<br></strong>Start to shop <strong>⟶</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner2.jpg","id":437,"dimRatio":0,"minHeight":680,"style":{"spacing":{"padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}}}} -->
+<div class="wp-block-cover" style="padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px;min-height:680px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-437" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner2.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-left has-large-font-size">JEANS &amp;<br>DENIM</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Start to shop <strong>⟶</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner3.jpg","id":438,"dimRatio":0,"minHeight":680,"className":"has-custom-content-position is-position-bottom-left ek-linked-block ek-linked-block-animate"} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-left ek-linked-block ek-linked-block-animate" style="min-height:680px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-438" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/banner3.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"backgroundColor":"black","textColor":"background"} -->
+<p class="has-background-color has-black-background-color has-text-color has-background"><strong>Accessories</strong><br>Start to shop <strong>⟶</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"large"} -->
+<h3 class="has-text-align-center has-primary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:600">Best seller of this month</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/triangle.png","id":465,"dimRatio":0,"focalPoint":{"x":"0.00","y":"0.50"},"isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-465" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/triangle.png" style="object-position:0% 50%" data-object-fit="cover" data-object-position="0% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"color":{}},"layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull"><!-- wp:woocommerce/product-new {"columns":5,"rows":1,"alignButtons":true,"contentVisibility":{"image":true,"title":true,"price":true,"rating":false,"button":false}} /-->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":"140px"}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":441,"width":61,"height":61,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/buysafe.png" alt="" class="wp-image-441" width="61" height="61"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary"} -->
+<p class="has-text-align-center has-primary-color has-text-color" style="font-style:normal;font-weight:600">Buy safe: our 30 days money back guarantee</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":450,"width":70,"height":52,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/freedelivery.png" alt="" class="wp-image-450" width="70" height="52"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary"} -->
+<p class="has-text-align-center has-primary-color has-text-color" style="font-style:normal;font-weight:600">Free delivery<br>for orders over 100$</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":451,"width":62,"height":56,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/customercare.png" alt="" class="wp-image-451" width="62" height="56"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary"} -->
+<p class="has-text-align-center has-primary-color has-text-color" style="font-style:normal;font-weight:600">Contact our Customer Care for any question or doubt</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"large"} -->
+<h3 class="has-text-align-center has-primary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:600">This week’s deals</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:woocommerce/product-new {"columns":5,"rows":1,"alignButtons":true,"contentVisibility":{"image":true,"title":true,"price":true,"rating":false,"button":false}} /-->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:cover {"url":"http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/wardrobecapsule-scaled.jpg","id":455,"dimRatio":0,"isDark":false} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-455" alt="" src="http://blocktheme.test/wonder/wp-content/uploads/sites/3/2022/07/wardrobecapsule-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"style":{"spacing":{"padding":{"top":"100px","right":"100px","bottom":"0px","left":"100px"},"blockGap":"100px"}}} -->
+<div class="wp-block-columns" style="padding-top:100px;padding-right:100px;padding-bottom:0px;padding-left:100px"><!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","right":"50px","bottom":"100px","left":"50px"}}},"backgroundColor":"tertiary","textColor":"background"} -->
+<div class="wp-block-group has-background-color has-tertiary-background-color has-text-color has-background" style="padding-top:50px;padding-right:50px;padding-bottom:100px;padding-left:50px"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"x-large"} -->
+<h2 class="has-x-large-font-size" style="font-style:normal;font-weight:500">BUILD YOUR CAPSULE WARDROBE</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Cras dolor mi, mattis nec placerat at, dapibus auctor mauris. Integer vel velit semper, aliquam dui vitae, ultrices odio. Vivamus in nunc sed arcu congue tincidunt ut eu est. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Aliquam id nibh pulvinar, accumsan metus nec, tempus leo. Vestibulum feugiat velit lacus, in dictum dolor ultricies at. Integer vitae vehicula erat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"background","textColor":"tertiary","style":{"border":{"radius":"0px"}},"className":"is-style-fill","fontSize":"normal"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill has-normal-font-size"><a class="wp-block-button__link has-tertiary-color has-background-background-color has-text-color has-background" style="border-radius:0px"><strong>START TO SHOP</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:html -->
+<div style="margin-bottom:-150px"></div>
+<!-- /wp:html --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":"200px"} -->
+<div style="height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
