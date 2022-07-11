0e9c3d6dcb2de29a9dd673290eb7230e7c13fb39
francgrasso <francgrasso@gmail.com>
2022-07-11 11:00:17 +0200

    new block image "creative" style

--- inc/register-block-styles.php
+++ inc/register-block-styles.php
@@ -21,6 +21,13 @@ function wonder_register_block_styles() {
 			'label' => esc_html__( 'Button Style 1', 'wonder' ),
 		)
 	);
+	register_block_style(
+		'core/image',
+		array(
+			'name'  => 'wonder-image-creative',
+			'label' => esc_html__( 'Creative', 'wonder' ),
+		)
+	);
 }
 
 add_action( 'init', 'wonder_register_block_styles' );
