eaa7c52ec37686e0a53ef445d7c94a575a3c5801
francgrasso <francgrasso@gmail.com>
2022-07-12 14:02:53 +0200

    add grunt and SASS

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+		sass: {
+			dev: {
+				files: [{
+                    expand: true,
+                    cwd: 'assets/sass',
+                    src: ['*.scss', 'blocks/*.scss'],
+                    dest: 'assets/css',
+                    ext: '.css'
+                }],
+			},
+			dist: {
+				files: [{
+                    expand: true,
+                    cwd: 'assets/sass',
+                    src: ['*.scss', 'blocks/*.scss'],
+                    dest: 'assets/css',
+                    ext: '.css'
+                }],
+				options: { 'no-source-map': '' }
+			},
+
+		},
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/css',
+                    src: ['*.css', '!*.min.css', 'blocks/*.css', 'blocks/!*.min.css'],
+                    dest: 'assets/css',
+                    ext: '.min.css'
+                }],
+				options: { 'sourceMap': false }
+            }
+        },
+        watch: {
+			css: {
+				files: ['assets/sass/*.scss', 'assets/sass/blocks/*.scss'],
+				tasks: ['sass:dev'],
+			},
+		},
+	});
+	
+	
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', ['sass:dev']);
+	grunt.registerTask('css', ['sass:dist', 'cssmin']);
+	grunt.registerTask('dist', ['css']);
+};
