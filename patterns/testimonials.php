40e04acbf73795ffe6cf8226906d8a9a66d6cf30
francgrasso <francgrasso@gmail.com>
2022-07-19 17:16:08 +0200

    testimonials page template

---
+++ patterns/testimonials.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Title: Testimonials page
+ * Slug: wonder/testimonials-page
+ * Categories: wonder-pages
+ * Block Types: core/post-content
+ *
+ * @package wonder
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:group {"layout":{"contentSize":"900px"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"130px"} -->
+<div style="height:130px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":"80px"}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"large"} -->
+<h2 class="has-large-font-size" style="font-style:normal;font-weight:600">Bringing value between different brands</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","right":"35px","bottom":"35px","left":"35px"}}},"backgroundColor":"secondary-background","textColor":"secondary-foreground"} -->
+<div class="wp-block-group has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:35px;padding-right:35px;padding-bottom:35px;padding-left:35px"><!-- wp:quote {"className":"is-style-wonder-quote-with-icon","fontSize":"medium"} -->
+<blockquote class="wp-block-quote is-style-wonder-quote-with-icon has-medium-font-size"><p>Here I can find original items, low prices without to lose quality or sustainability. Love it!</p><cite>MARTHA RAMOS</cite></blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"right","width":262,"height":308,"sizeSlug":"full","linkDestination":"none","className":"is-style-wonder-image-creative"} -->
+<figure class="wp-block-image alignright size-full is-resized is-style-wonder-image-creative"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/testimonials/testimonials-image-3.png" alt="" width="262" height="308"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:html -->
+<div style="margin-bottom: -150px"></div>
+<!-- /wp:html --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","right":"35px","bottom":"35px","left":"35px"}}},"backgroundColor":"secondary-background","textColor":"secondary-foreground"} -->
+<div class="wp-block-group has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:35px;padding-right:35px;padding-bottom:35px;padding-left:35px"><!-- wp:quote {"className":"is-style-wonder-quote-with-icon","fontSize":"medium"} -->
+<blockquote class="wp-block-quote is-style-wonder-quote-with-icon has-medium-font-size"><p>I don't usually leave reviews but I have decided to for Wonder. I love there clothing and accessories I've never had a problem returning and parcels always arrive well within time stated.</p><cite>JOCELYN HENDRIKSON</cite></blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"right","width":264,"height":305,"sizeSlug":"full","linkDestination":"none","className":"is-style-wonder-image-creative-alt"} -->
+<figure class="wp-block-image alignright size-full is-resized is-style-wonder-image-creative-alt"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/testimonials/testimonials-image-4.png" alt="" width="264" height="305"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:html -->
+<div style="margin-bottom: -150px"></div>
+<!-- /wp:html --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","right":"35px","bottom":"35px","left":"35px"}}},"backgroundColor":"secondary-background","textColor":"secondary-foreground"} -->
+<div class="wp-block-group has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:35px;padding-right:35px;padding-bottom:35px;padding-left:35px"><!-- wp:quote {"className":"is-style-wonder-quote-with-icon","fontSize":"medium"} -->
+<blockquote class="wp-block-quote is-style-wonder-quote-with-icon has-medium-font-size"><p>First order and will be back! Ordered 4 items and couldn't be happier with choices. Amazing value, will definitely be ordering more.</p><cite>PAULA DRUM</cite></blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"right","width":262,"height":304,"sizeSlug":"full","linkDestination":"none","className":"is-style-wonder-image-creative"} -->
+<figure class="wp-block-image alignright size-full is-resized is-style-wonder-image-creative"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/testimonials/testimonials-image-2.png" alt="" width="262" height="304"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:html -->
+<div style="margin-bottom: -150px"></div>
+<!-- /wp:html --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","right":"35px","bottom":"35px","left":"35px"}}},"backgroundColor":"secondary-background","textColor":"secondary-foreground"} -->
+<div class="wp-block-group has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:35px;padding-right:35px;padding-bottom:35px;padding-left:35px"><!-- wp:quote {"className":"is-style-wonder-quote-with-icon","fontSize":"medium"} -->
+<blockquote class="wp-block-quote is-style-wonder-quote-with-icon has-medium-font-size"><p>I'm super happy with the quality and sizing of my items. Such good value for money! So stylish too. I will definitely be purchasing again!</p><cite>MIKE HANSEN</cite></blockquote>
+<!-- /wp:quote -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"right","width":264,"height":303,"sizeSlug":"full","linkDestination":"none","className":"is-style-wonder-image-creative"} -->
+<figure class="wp-block-image alignright size-full is-resized is-style-wonder-image-creative"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/testimonials/testimonials-image-1.png" alt="" width="264" height="303"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:html -->
+<div style="margin-bottom: -150px"></div>
+<!-- /wp:html --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
