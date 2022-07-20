c439ccfa0db5a013dd29621334899ec983f0a4de
francgrasso <francgrasso@gmail.com>
2022-07-20 13:19:52 +0200

    Improve header default block spacing

--- patterns/site-header-left-logo-navigation-inline.php
+++ patterns/site-header-left-logo-navigation-inline.php
@@ -11,22 +11,34 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"},"blockGap":"30px"}},"backgroundColor":"secondary-background","textColor":"secondary-foreground","layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:30px;padding-bottom:30px"><!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:site-logo {"width":150} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"}}},"backgroundColor":"secondary-background","textColor":"secondary-foreground","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background" style="padding-top:30px;padding-bottom:30px">
+	<!-- wp:group {"style":{"spacing":{"blockGap":"50px"}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"30px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<div class="wp-block-group">
+			<!-- wp:site-logo {"width":150} /-->
+			<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group">
+				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
+				<div class="wp-block-group">
+					<!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary-foreground"}}}}} /-->
+				</div>
+				<!-- /wp:group -->
+				<!-- wp:site-tagline /-->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
 
-<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary-foreground"}}}}} /--></div>
-<!-- /wp:group -->
+		<!-- wp:navigation {"textColor":"secondary-foreground","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600"},"spacing":{"blockGap":"30px"}},"fontSize":"x-small"} /-->
 
-<!-- wp:site-tagline /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
-
-<!-- wp:navigation {"ref":811,"textColor":"secondary-foreground","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600"},"spacing":{"blockGap":"30px"}},"fontSize":"x-small"} /-->
-
-<!-- wp:woocommerce/mini-cart {"addToCartBehaviour":"open_drawer","fontSize":"x-small"} /--></div>
-<!-- /wp:group --></div>
+		<!-- wp:group {"style":{"spacing":{"blockGap":"30px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+		<div class="wp-block-group">
+			<!-- wp:woocommerce/mini-cart {"addToCartBehaviour":"open_drawer","fontSize":"x-small"} /-->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
 <!-- /wp:group -->
