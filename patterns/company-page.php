34226474c05b9a737d2d5888fbf0284714f22256
francgrasso <francgrasso@gmail.com>
2022-07-21 12:16:54 +0200

    new Company page template

---
+++ patterns/company-page.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Title: Company page
+ * Slug: wonder/company-page
+ * Categories: wonder-pages
+ * Block Types: core/post-content
+ *
+ * @package wonder
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":"90px"}}} -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{}}} -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-1.jpg","dimRatio":0,"contentPosition":"bottom center","isDark":false,"style":{"spacing":{"padding":{"top":"50px","right":"50px","bottom":"0px","left":"50px"}}}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-center" style="padding-top:50px;padding-right:50px;padding-bottom:0px;padding-left:50px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-1.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"0px","left":"30px"}}},"backgroundColor":"background","textColor":"foreground"} -->
+<div class="wp-block-group has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:0px;padding-left:30px"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">Let’s be fair</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Because fashion need to be fair, always</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{}}} -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-2.jpg","dimRatio":0,"contentPosition":"bottom center","style":{"spacing":{"padding":{"top":"50px","right":"50px","bottom":"0px","left":"50px"}}}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="padding-top:50px;padding-right:50px;padding-bottom:0px;padding-left:50px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-2.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"0px","left":"30px"}}},"backgroundColor":"background","textColor":"foreground"} -->
+<div class="wp-block-group has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:0px;padding-left:30px"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">Let’s be for all</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Discover why and how our shop is totally inclusive</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{}}} -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-3.jpg","dimRatio":0,"contentPosition":"bottom center","isDark":false,"style":{"spacing":{"padding":{"top":"50px","right":"50px","bottom":"0px","left":"50px"}}}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-center" style="padding-top:50px;padding-right:50px;padding-bottom:0px;padding-left:50px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/company-3.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"0px","left":"30px"}}},"backgroundColor":"background","textColor":"foreground"} -->
+<div class="wp-block-group has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:0px;padding-left:30px"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">Let’s clean up</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Discover our sustainability initiatives</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"large"} -->
+<p class="has-primary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:600">We <strong><em>really</em> </strong>love our tribu.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"medium"} -->
+<p class="has-medium-font-size" style="font-style:normal;font-weight:500">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. <br>Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"800"}},"fontSize":"medium"} -->
+<p class="has-medium-font-size" style="font-style:normal;font-weight:800"><a href="#"><?php esc_html_e( 'Read all testimonials &gt;', 'wonder' ); ?></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/testimonial.jpg","dimRatio":50,"focalPoint":{"x":"0.50","y":"0.00"},"minHeight":700,"customGradient":"linear-gradient(90deg,rgb(0,167,156) 0%,rgb(1,175,141) 21%,rgba(255,255,255,0) 21%)","contentPosition":"bottom center","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;min-height:700px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(90deg,rgb(0,167,156) 0%,rgb(1,175,141) 21%,rgba(255,255,255,0) 21%)"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/testimonial.jpg" style="object-position:50% 0%" data-object-fit="cover" data-object-position="50% 0%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","right":"70px","bottom":"0px","left":"70px"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"backgroundColor":"background","textColor":"foreground","layout":{"inherit":false}} -->
+<div class="wp-block-group has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:50px;padding-right:70px;padding-bottom:0px;padding-left:70px;font-style:normal;font-weight:500"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><strong>Martha Ramos<br></strong>Customer from 2007</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","width":"33.33%","className":"negative-left-margin"} -->
+<div class="wp-block-column is-vertically-aligned-bottom negative-left-margin" style="flex-basis:33.33%"><!-- wp:group {"style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"backgroundColor":"secondary-background","textColor":"secondary-foreground","fontSize":"x-large"} -->
+<div class="wp-block-group has-secondary-foreground-color has-secondary-background-background-color has-text-color has-background has-x-large-font-size" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;font-style:normal;font-weight:700"><!-- wp:paragraph -->
+<p>“Here I can find original items, low prices without to lose quality or sustainability. Love it!“</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":"140px"}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/high-quality.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">High quality and low cost</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"normal"} -->
+<p class="has-text-align-center has-normal-font-size" style="font-style:normal;font-weight:600">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/happycustomers.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">50K Happy Customers</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"normal"} -->
+<p class="has-text-align-center has-normal-font-size" style="font-style:normal;font-weight:600">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/company/refund-policy.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary","fontSize":"medium"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-medium-font-size" style="font-style:normal;font-weight:600">A transparent Refund policy</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"normal"} -->
+<p class="has-text-align-center has-normal-font-size" style="font-style:normal;font-weight:600">Fusce gravida ut nisi et facilisis. Nullam ut mi fermentum, posuere dolor id, ultricies ipsum. Duis urna ipsum, tincidunt ut lorem.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
