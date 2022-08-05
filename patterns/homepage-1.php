95fbcf004ed93bf5fe61bfec273ce64eee64623a
francgrasso <francgrasso@gmail.com>
2022-08-05 14:31:49 +0200

    fix escape function in template

--- patterns/homepage-1.php
+++ patterns/homepage-1.php
@@ -198,7 +198,7 @@
 <div class="wp-block-cover alignfull" style="padding-top:100px;padding-right:100px;padding-bottom:0px;padding-left:100px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/home-1/testimonials-bg.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":true}} -->
 <div class="wp-block-group"><!-- wp:media-text {"align":"","mediaPosition":"right","mediaType":"image"} -->
 <div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/demo-content/home-1/martharamos.jpg" alt=""/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"huge"} -->
-<p class="has-huge-font-size" style="font-style:normal;font-weight:700"><?php echo esc_html_x( '“ Here I can find original items, low prices without to lose quality or sustainability.<br>Love it! “', 'demo-contents', 'yith-wonder' ); ?></p>
+<p class="has-huge-font-size" style="font-style:normal;font-weight:700"><?php echo wp_kses_post( _x( '“ Here I can find original items, low prices without to lose quality or sustainability.<br>Love it! “', 'demo-contents', 'yith-wonder' ) ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:spacer {"height":"50px"} -->
