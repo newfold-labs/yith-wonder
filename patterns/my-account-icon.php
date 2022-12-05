b9ec7d2e7ed8e0a910ff9a78a99342967c3a95e1
arunshenoy99 <devarunshenoy99@gmail.com>
2022-12-05 14:18:04 +0530

    use get_template_directory_uri for fetching the account icon

--- patterns/my-account-icon.php
+++ patterns/my-account-icon.php
@@ -11,5 +11,5 @@
 
 ?>
 <!-- wp:image {"sizeSlug":"full","linkDestination":"custom","className":"yith-wonder-my-account-login-icon"} -->
-<figure class="wp-block-image size-full yith-wonder-my-account-login-icon"><a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/assets/images/icons/post-author.svg" alt=""></a></figure>
+<figure class="wp-block-image size-full yith-wonder-my-account-login-icon"><a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icons/post-author.svg" alt=""></a></figure>
 <!-- /wp:image -->
