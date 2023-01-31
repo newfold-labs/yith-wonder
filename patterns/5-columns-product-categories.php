90e13ec26126bedbfe3858eb2adf41b44fbe1af7
francgrasso <francgrasso@gmail.com>
2023-01-31 17:30:48 +0100

    add "5-columns-product-categories" pattern

---
+++ patterns/5-columns-product-categories.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Title: 5 Columns product categories banner
+ * Slug: yith-wonder/5-columns-product-categoriers-banner
+ * Categories: yith-wonder-banners
+ * Keywords: banners
+ * Block Types: core/group, core/columns, core/image, core/cover
+ *
+ * @package yith-wonder
+ * @since 1.0.0
+ */
+
+?>
+
+<!-- wp:heading {"textAlign":"center","textColor":"contrast"} -->
+<h2 class="has-text-align-center has-contrast-color has-text-color"><?php echo esc_html_x( 'Our new collection', 'demo-contents', 'yith-wonder' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"backgroundColor":"secondary"} -->
+<hr class="wp-block-separator has-text-color has-secondary-color has-alpha-channel-opacity has-secondary-background-color has-background"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":"50px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":"bottom"} -->
+<div class="wp-block-columns are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":1539,"width":50,"height":65,"sizeSlug":"large","linkDestination":"custom"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><a href="#"><img src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/rings-platinum.svg" alt="" class="wp-image-1539" width="50" height="65"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary","fontSize":"medium","fontFamily":"playfair"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-playfair-font-family has-medium-font-size"><?php echo esc_attr_x( 'PLATINIUM', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo esc_attr_x( 'RINGS', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":1538,"width":50,"height":65,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/rings-pearl.svg" alt="" class="wp-image-1538" width="50" height="65"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary","fontSize":"medium","fontFamily":"playfair"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-playfair-font-family has-medium-font-size"><?php echo esc_attr_x( 'PEARL', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo esc_attr_x( 'RINGS', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":1537,"width":70,"height":65,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/rings-engagment.svg" alt="" class="wp-image-1537" width="70" height="65"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary","fontSize":"medium","fontFamily":"playfair"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-playfair-font-family has-medium-font-size"><?php echo esc_attr_x( 'ENGAGEMENT', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo esc_attr_x( 'RINGS', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":1536,"width":60,"height":65,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/rings_diamond.svg" alt="" class="wp-image-1536" width="60" height="65"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary","fontSize":"medium","fontFamily":"playfair"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-playfair-font-family has-medium-font-size"><?php echo esc_attr_x( 'DIAMOND', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo esc_attr_x( 'RINGS', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":1540,"width":60,"height":65,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/rings-rose-gold.svg" alt="" class="wp-image-1540" width="60" height="65"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","textColor":"primary","fontSize":"medium","fontFamily":"playfair"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-playfair-font-family has-medium-font-size"><?php echo esc_attr_x( 'ROSE GOLD', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo esc_attr_x( 'RINGS', 'demo-contents', 'yith-wonder' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
