9bfe678c38c170ed77f1c272c98f1022957a86f1
francgrasso <francgrasso@gmail.com>
2023-02-01 12:24:48 +0100

    remove not needed typography properties #4

--- patterns/creative-banners-grid.php
+++ patterns/creative-banners-grid.php
@@ -17,9 +17,9 @@
 <div class="wp-block-column" style="flex-basis:45%"><!-- wp:cover {"url":"http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-12.jpg","id":1514,"dimRatio":0,"minHeight":100,"minHeightUnit":"%","isDark":false} -->
 <div class="wp-block-cover is-light" style="min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1514" alt="" src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-12.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"70%"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"color":{"background":"#ffffffc9"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"fontSize":"x-large","fontFamily":"playfair"} -->
-<p class="has-playfair-font-family has-x-large-font-size"><?php echo esc_attr_x( 'Watches', 'demo-contents', 'yith-wonder' ); ?></p>
-<!-- /wp:paragraph -->
+<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"level":3,"fontSize":"x-large"} -->
+<h3 class="has-x-large-font-size"><?php echo esc_attr_x( 'Watches', 'demo-contents', 'yith-wonder' ); ?></h3>
+<!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
 <p class="has-small-font-size"><?php echo esc_attr_x( 'NEW COLLECTIONS', 'demo-contents', 'yith-wonder' ); ?></p>
@@ -37,9 +37,9 @@
 <div class="wp-block-column" style="flex-basis:55%"><!-- wp:cover {"url":"http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-11.jpg","id":1513,"dimRatio":0,"isDark":false} -->
 <div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1513" alt="" src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-11.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"70%"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"color":{"background":"#ffffffc9"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"align":"center","fontSize":"x-large","fontFamily":"playfair"} -->
-<p class="has-text-align-center has-playfair-font-family has-x-large-font-size"><?php echo esc_attr_x( 'Accessories', 'demo-contents', 'yith-wonder' ); ?></p>
-<!-- /wp:paragraph --></div>
+<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"x-large"} -->
+<h3 class="has-text-align-center has-x-large-font-size"><?php echo esc_attr_x( 'Accessories', 'demo-contents', 'yith-wonder' ); ?></h3>
+<!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
@@ -47,9 +47,9 @@
 <!-- wp:cover {"url":"http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-09.jpg","id":1501,"dimRatio":0,"isDark":false} -->
 <div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1501" alt="" src="http://wonder.test/jewelry/wp-content/uploads/sites/3/2023/01/prodotto-09.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"70%"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"color":{"background":"#ffffffc9"},"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"align":"center","fontSize":"x-large","fontFamily":"playfair"} -->
-<p class="has-text-align-center has-playfair-font-family has-x-large-font-size"><?php echo esc_attr_x( 'Jewelry', 'demo-contents', 'yith-wonder' ); ?></p>
-<!-- /wp:paragraph --></div>
+<div class="wp-block-group has-background" style="background-color:#ffffffc9;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"x-large"} -->
+<h3 class="has-text-align-center has-x-large-font-size"><?php echo esc_attr_x( 'Jewelry', 'demo-contents', 'yith-wonder' ); ?></h3>
+<!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover --></div>
