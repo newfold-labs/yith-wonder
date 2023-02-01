f3dc70f8b6d5a466b6a71f38ec3f07a0d3a4b40c
francgrasso <francgrasso@gmail.com>
2023-02-01 11:54:46 +0100

    remove not needed typography properties #1

--- patterns/banner-with-title-and-separator.php
+++ patterns/banner-with-title-and-separator.php
@@ -20,16 +20,16 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center"} -->
 <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"35%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:35%"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
-<h2 style="font-style:normal;font-weight:400"><?php echo esc_html_x( 'Engagement Rings', 'demo-contents', 'yith-wonder' ); ?></h2>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:35%"><!-- wp:heading -->
+<h2><?php echo esc_html_x( 'Engagement Rings', 'demo-contents', 'yith-wonder' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"secondary","className":"is-style-yith-wonder-left-aligned-separator"} -->
 <hr class="wp-block-separator has-text-color has-secondary-color has-alpha-channel-opacity has-secondary-background-color has-background is-style-yith-wonder-left-aligned-separator" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"/>
 <!-- /wp:separator -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
-<p style="font-style:normal;font-weight:400"><?php echo wp_kses_post( _x( 'Lorem ipsum dolor sit amet, consectetur adispising elit.<br>Vestibulum varius, mi ut posuere iaculis, orci risus consequat urna, faucibus laoreet nunc just a quam.', 'demo-contents', 'yith-wonder' ) ); ?></p>
+<!-- wp:paragraph -->
+<p><?php echo wp_kses_post( _x( 'Lorem ipsum dolor sit amet, consectetur adispising elit.<br>Vestibulum varius, mi ut posuere iaculis, orci risus consequat urna, faucibus laoreet nunc just a quam.', 'demo-contents', 'yith-wonder' ) ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
