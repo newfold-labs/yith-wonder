34509135837e20eb3c1580830f72f1acf60cc04f
francgrasso <francgrasso@gmail.com>
2023-06-19 11:12:04 +0200

    move CSS autoprefix to grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,66 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
+	grunt.initConfig({
 		sass: {
 			dev: {
 				files: [{
-                    expand: true,
-                    cwd: 'assets/sass',
-                    src: ['*.scss', 'blocks/*.scss', 'woocommerce/woocommerce.scss'],
-                    dest: 'assets/css',
-                    ext: '.css'
-                }],
+					expand: true,
+					cwd: 'assets/sass',
+					src: ['*.scss', 'blocks/*.scss', 'woocommerce/woocommerce.scss'],
+					dest: 'assets/css',
+					ext: '.css'
+				}],
 			},
 			dist: {
 				files: [{
-                    expand: true,
-                    cwd: 'assets/sass',
-                    src: ['*.scss', 'blocks/*.scss', 'woocommerce/woocommerce.scss'],
-                    dest: 'assets/css',
-                    ext: '.css'
-                }],
+					expand: true,
+					cwd: 'assets/sass',
+					src: ['*.scss', 'blocks/*.scss', 'woocommerce/woocommerce.scss'],
+					dest: 'assets/css',
+					ext: '.css'
+				}],
 				options: { 'no-source-map': '' }
 			},
 
 		},
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: 'assets/css',
-                    src: ['*.css', '!*.min.css', 'blocks/*.css', 'blocks/!*.min.css', 'woocommerce/*.css', 'woocommerce/!*.min.css'],
-                    dest: 'assets/css',
-                    ext: '.min.css'
-                }],
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'assets/css',
+					src: ['*.css', '!*.min.css', 'blocks/*.css', 'blocks/!*.min.css', 'woocommerce/*.css', 'woocommerce/!*.min.css'],
+					dest: 'assets/css',
+					ext: '.min.css'
+				}],
 				options: { 'sourceMap': false }
-            }
-        },
-        watch: {
+			}
+		},
+		watch: {
 			css: {
 				files: ['assets/sass/*.scss', 'assets/sass/blocks/*.scss', 'assets/sass/woocommerce/*.scss'],
 				tasks: ['sass:dev'],
 			},
 		},
-        clean: ['assets/css/**/*.min.css']
+		autoprefixer: {
+			options: {
+				processors: [
+					require('autoprefixer')(),
+				]
+			},
+			dist: {
+				src: 'assets/css/**/*.css'
+			}
+		},
+		clean: ['assets/css/**/*.min.css']
 	});
 	
 	
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	
-	grunt.registerTask('default', ['sass:dev']);
-	grunt.registerTask('css', ['sass:dist', 'clean', 'cssmin']);
+	grunt.registerTask('default', ['sass:dev', 'autoprefixer']);
+	grunt.registerTask('css', ['sass:dist', 'autoprefixer', 'clean', 'cssmin']);
 	grunt.registerTask('dist', ['css']);
 };
