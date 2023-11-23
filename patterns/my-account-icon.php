be5426d9aaca495b9b9f56ea815db51c6b30fdec
francgrasso <francgrasso@gmail.com>
2023-11-23 15:22:53 +0100

    solve isssue #16 by @carolinan

--- patterns/my-account-icon.php
+++ patterns/my-account-icon.php
@@ -1,15 +1,24 @@
 <?php
 /**
- * Title: My-Account icon
- * Slug: yith-wonder/my-account-icon
- * Categories: yith-wonder-utilities
- * Block Types: core/image
+ * My Account pattern. This will be registered only if WooCommerce is enabled.
  *
  * @package yith-wonder
  * @since 1.0.0
  */
 
-?>
-<!-- wp:image {"sizeSlug":"full","linkDestination":"custom","className":"yith-wonder-my-account-login-icon"} -->
-<figure class="wp-block-image size-full yith-wonder-my-account-login-icon"><a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icons/post-author.svg" alt=""></a></figure>
-<!-- /wp:image -->
+if ( class_exists( 'woocommerce' ) ) :
+	/**
+	 * Title: My-Account icon
+	 * Slug: yith-wonder/my-account-icon
+	 * Categories: yith-wonder-utilities
+	 * Block Types: core/image
+	 */
+
+	?>
+	<!-- wp:image {"sizeSlug":"full","linkDestination":"custom","className":"yith-wonder-my-account-login-icon"} -->
+	<figure class="wp-block-image size-full yith-wonder-my-account-login-icon"><a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icons/post-author.svg" alt=""></a></figure>
+	<!-- /wp:image -->
+
+	<?php
+
+endif;
