20115c6ddd7ab54abca81e3da38616058272411a
francgrasso <francgrasso@gmail.com>
2024-05-16 14:45:23 +0200

    add transparent header

--- assets/js/block-variations.js
+++ assets/js/block-variations.js
@@ -3,7 +3,7 @@ const { __ } = wp.i18n;
 /* Default header. */
 wp.blocks.registerBlockVariation( 'core/template-part', {
 	name: 'header-default',
-	title: 'Header default',
+	title: __( 'Default style' ),
 	description: __( 'Default style' ),
 	scope: [ 'transform' ],
 	attributes: {
@@ -14,8 +14,8 @@ wp.blocks.registerBlockVariation( 'core/template-part', {
 /* Sticky header. */
 wp.blocks.registerBlockVariation( 'core/template-part', {
 	name: 'header-sticky',
-	title: 'Header sticky',
-	description: __( 'Sticky header.' ),
+	title: __( 'Sticky header' ),
+	description: __( 'Sticky header' ),
 	scope: [ 'transform' ],
 	attributes: {
 		className: 'is-style-yith-wonder-sticky-header',
@@ -25,8 +25,8 @@ wp.blocks.registerBlockVariation( 'core/template-part', {
 /* Transparent header. */
 wp.blocks.registerBlockVariation( 'core/template-part', {
 	name: 'header-overlay',
-	title: 'Header overlay',
-	description: __( 'Overlay header.' ),
+	title: __( 'Overlay header' ),
+	description: __( 'Overlay header' ),
 	scope: [ 'transform' ],
 	attributes: {
 		className: 'is-style-yith-wonder-overlay-header',
